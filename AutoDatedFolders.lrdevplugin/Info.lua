6b04881b61c277bad1521fb8d00258528edc99a1
Colin <colin@BOXdev.com>
2023-04-28 16:06:44 -0400

    Initial vanilla manifest

---
+++ AutoDatedFolders.lrdevplugin/Info.lua
@@ -0,0 +1,10 @@
+return {
+  VERSION = { major=0, minor=1, revision=0, },
+
+  LrSdkVersion = 12.3,
+  -- LrSdkMinimumVersion = 4.0,
+
+  LrToolkitIdentifier = "com.boxdev.auto-dated-folders",
+  LrPluginName = "Auto Dated Folders",
+  LrPluginInfoUrl="https://wreckreation.org",
+}
